6c29b1c497cbbb35407c7a0174272328236b2378
Josh Bangerter <joshbang@me.com>
2016-11-27 20:43:38 -0700

    underscore

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,13 +18,7 @@ app.get('/todos', function(req, res, next) {
 })
 app.get('/todos/:id', function(req, res, next) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
-
-  todos.forEach(function(todo) {
-    if(todoId === todo.id) {
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId})
   if (matchedTodo) {
     res.json(matchedTodo)
   } else {
@@ -35,7 +29,15 @@ app.get('/todos/:id', function(req, res, next) {
 
 
 app.post('/todos', function(req, res, next) {
-  var body = req.body;
+
+  //body will only allow keys that are specified to be posted
+  var body = _.pick(req.body, 'description', 'completed');
+  //bad request
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+  //set body.description to be trimmed value
+  body.description = body.description.trim();
 
   body.id = todoNextId++;
 
